6bc6f17add66f3f4beebf2fe111932830e4761d0
GitHubDroid <promptnet@promptnetworks.com>
2018-10-03 17:29:13 -0400

    Add files via upload

---
+++ miner/miner_config.py
@@ -0,0 +1,13 @@
+"""Configure this file before you start mining. Check wallet.py for
+more details.
+"""
+
+# Write your generated adress here. All coins mined will go to this address
+MINER_ADDRESS = "ZX8I2Lgo3D6sRyQFaCa3pmwb0FeUE7cd9q1nDk39HjzrtmG0gpq0sUgZFqcFxbbJGan513lPgs0PGHzXlox0Vg=="
+
+# Write your node url or ip. If you are running it localhost use default
+MINER_NODE_URL = "http://localhost:5000"
+
+# Store the url data of every other node in the network
+# so that we can communicate with them
+PEER_NODES = []
